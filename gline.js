0fc5e840f37b712b3e7681c31e19fee802383594
Rahul Vaghela <vaghelarahul94@gmail.com>
2017-07-19 00:38:19 -0700

    First commit

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','indiana.edu','cam.ac.uk','mac.com','uct.ac.za','berkeley.edu','unicon.net','gmail.com','ucdavis.edu','stanford.edu'],
+['2005',57,12,7,12,14,12,6,10,11,4],
+['2006',892,424,405,352,304,282,287,225,218,164]
+];
